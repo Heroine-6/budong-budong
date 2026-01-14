34078d5ee7052ebd22782aae3294715848b8cc8b
i-am-dahee <hxxly02@gmail.com>
2026-01-14 18:02:29 +0900

    feat: TimeFormatUtil 클래스 생성

---
+++ src/main/java/com/example/budongbudong/common/util/TimeFormatUtil.java
@@ -0,0 +1,34 @@
+package com.example.budongbudong.common.util;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class TimeFormatUtil {
+
+    private TimeFormatUtil() { }
+
+    public static String formatTime(LocalDateTime time) {
+
+        if (time == null) {
+            return "";
+        }
+
+        Duration duration = Duration.between(time, LocalDateTime.now());
+        long minutes = duration.toMinutes();
+
+        if (minutes < 1) {
+            return "방금 전";
+        }
+        if (minutes < 60) {
+            return minutes + "분 전";
+        }
+
+        long hours = duration.toHours();
+        if (hours < 24) {
+            return hours + "시간 전";
+        }
+
+        long days = duration.toDays();
+        return days + "일 전";
+    }
+}
