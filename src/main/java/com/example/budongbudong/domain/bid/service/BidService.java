42b8542b0efb0c42ad519b63a86d0dee954d8060
i-am-dahee <hxxly02@gmail.com>
2026-01-14 20:00:54 +0900

    feat: 내 입찰 내역 조회 기능 구현

--- src/main/java/com/example/budongbudong/domain/bid/service/BidService.java
+++ src/main/java/com/example/budongbudong/domain/bid/service/BidService.java
@@ -8,6 +8,7 @@ import com.example.budongbudong.domain.auction.repository.AuctionRepository;
 import com.example.budongbudong.domain.bid.dto.request.CreateBidRequest;
 import com.example.budongbudong.domain.bid.dto.response.CreateBidResponse;
 import com.example.budongbudong.domain.bid.dto.response.ReadAllBidsResponse;
+import com.example.budongbudong.domain.bid.dto.response.ReadMyBidsResponse;
 import com.example.budongbudong.domain.bid.entity.Bid;
 import com.example.budongbudong.domain.bid.enums.BidStatus;
 import com.example.budongbudong.domain.bid.repository.BidRepository;
@@ -75,5 +76,15 @@ public class BidService {
         return bidRepository.findAllByAuctionId(auctionId, pageable)
                 .map(ReadAllBidsResponse::from);
 
+    }
+
+    /**
+     * 내 입찰 내역 조회
+     */
+    @Transactional(readOnly = true)
+    public Page<ReadMyBidsResponse> readMyBids(Long userId, Pageable pageable) {
+
+        return bidRepository.findMyBidsPage(userId, pageable)
+                .map(ReadMyBidsResponse::from);
     }
 }
