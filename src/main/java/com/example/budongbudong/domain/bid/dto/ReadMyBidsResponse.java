810b1564e3fc74280841b0821204c2cc02a27867
i-am-dahee <hxxly02@gmail.com>
2026-01-14 19:44:54 +0900

    feat: 내 입찰 내역 조회 Response 추가

---
+++ src/main/java/com/example/budongbudong/domain/bid/dto/ReadMyBidsResponse.java
@@ -0,0 +1,29 @@
+package com.example.budongbudong.domain.bid.dto;
+
+import com.example.budongbudong.domain.bid.entity.Bid;
+import com.example.budongbudong.domain.bid.enums.BidStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
+public class ReadMyBidsResponse {
+
+    private final Long propertyId;
+    private final BidStatus status;
+    private final String propertyName;
+    private final Long price;
+    private final LocalDateTime endedAt;
+
+    public static ReadMyBidsResponse from(Bid bid) {
+        return new ReadMyBidsResponse(
+                bid.getAuction().getProperty().getId(),
+                bid.getStatus(),
+                bid.getAuction().getProperty().getName(),
+                bid.getPrice(),
+                bid.getAuction().getEndedAt()
+        );
+    }
+}
