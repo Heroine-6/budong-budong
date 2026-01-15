245d1f79436b5a7f785802b9983105eeec434c7d
i-am-dahee <hxxly02@gmail.com>
2026-01-15 12:41:32 +0900

    refactor: 필요 없는 코드 제거

--- src/main/java/com/example/budongbudong/common/entity/BaseEntity.java
+++ src/main/java/com/example/budongbudong/common/entity/BaseEntity.java
@@ -3,10 +3,7 @@ package com.example.budongbudong.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
