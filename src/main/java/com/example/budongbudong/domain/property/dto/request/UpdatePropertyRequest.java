0cc4e0a15ce17684cbdcb3b72f28fc957a0ec909
i-am-dahee <hxxly02@gmail.com>
2026-01-15 12:00:41 +0900

    refactor: 필요 없는 코드 제거

--- src/main/java/com/example/budongbudong/domain/property/dto/request/UpdatePropertyRequest.java
+++ src/main/java/com/example/budongbudong/domain/property/dto/request/UpdatePropertyRequest.java
@@ -1,7 +1,6 @@
 package com.example.budongbudong.domain.property.dto.request;
 
 import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
