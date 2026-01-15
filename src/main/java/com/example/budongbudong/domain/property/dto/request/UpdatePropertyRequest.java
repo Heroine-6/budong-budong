0791e4fb514537fa410115e0338daa6cd1c4bdbd
i-am-dahee <hxxly02@gmail.com>
2026-01-15 12:00:19 +0900

    fix: @NotNull 제거

--- src/main/java/com/example/budongbudong/domain/property/dto/request/UpdatePropertyRequest.java
+++ src/main/java/com/example/budongbudong/domain/property/dto/request/UpdatePropertyRequest.java
@@ -12,11 +12,9 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class UpdatePropertyRequest {
 
-    @NotNull(message = "경매 시작가는 필수 입력값입니다.")
     @Positive(message = "경매 시작가는 0보다 커야 합니다.")
     private Long price;
 
-    @NotNull(message = "입주 가능 날짜는 필수 입력값입니다.")
     @Future(message = "입주 가능 날짜는 미래 날짜여야 합니다.")
     private LocalDate migrateDate;
 
