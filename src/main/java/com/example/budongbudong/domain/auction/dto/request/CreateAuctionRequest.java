7bcbf218dfc5945758df8a53c429e2b4c340b219
윤지현 <139779783+jheony@users.noreply.github.com>
2026-01-15 11:43:08 +0900

    refactor: Validation 적용

--- src/main/java/com/example/budongbudong/domain/auction/dto/request/CreateAuctionRequest.java
+++ src/main/java/com/example/budongbudong/domain/auction/dto/request/CreateAuctionRequest.java
@@ -1,5 +1,7 @@
 package com.example.budongbudong.domain.auction.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -7,9 +9,17 @@ import java.time.LocalDateTime;
 @Getter
 public class CreateAuctionRequest {
 
+    @NotBlank(message = "매물ID는 필수입니다.")
     private Long propertyId;
+
+    @NotBlank(message = "시작가는 필수입니다.")
+    @Positive(message = "시작가는 0보다 커야 합니다.")
     private Long startPrice;
+
+    @NotBlank(message = "경매 시작 시간은 필수입니다.")
     private LocalDateTime startedAt;
+
+    @NotBlank(message = "경매 종료 시간은 필수입니다.")
     private LocalDateTime endedAt;
 
 }
