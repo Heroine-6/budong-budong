bf01eb92917054e738f56b73eb7a06131736f6b7
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2026-01-16 10:16:15 +0900

    refactor: 타입 수정

--- src/main/java/com/example/budongbudong/domain/auctionwinner/repository/AuctionWinnerRepository.java
+++ src/main/java/com/example/budongbudong/domain/auctionwinner/repository/AuctionWinnerRepository.java
@@ -3,7 +3,7 @@ package com.example.budongbudong.domain.auctionwinner.repository;
 import com.example.budongbudong.domain.auctionwinner.entity.AuctionWinner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuctionWinnerRepository extends JpaRepository<AuctionWinner,Integer> {
+public interface AuctionWinnerRepository extends JpaRepository<AuctionWinner, Long> {
 
     boolean existsByAuctionId(Long id);
 }
