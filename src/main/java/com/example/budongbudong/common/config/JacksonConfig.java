73f16b7f20da0f49bce235764d39498b1de63e4b
i-am-dahee <hxxly02@gmail.com>
2026-01-16 11:54:50 +0900

    refactor: 필요 없는 코드 제거

--- src/main/java/com/example/budongbudong/common/config/JacksonConfig.java
+++ src/main/java/com/example/budongbudong/common/config/JacksonConfig.java
@@ -1,8 +1,5 @@
 package com.example.budongbudong.common.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
