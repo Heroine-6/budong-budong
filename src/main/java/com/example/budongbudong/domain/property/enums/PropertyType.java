1274054b5eaa514b54551ef1aab74ffa481a5e44
castlejuyeon <jucess1106@naver.com>
2026-01-19 10:17:27 +0900

    refactor: enum 상태 설명 추가

--- src/main/java/com/example/budongbudong/domain/property/enums/PropertyType.java
+++ src/main/java/com/example/budongbudong/domain/property/enums/PropertyType.java
@@ -1,7 +1,13 @@
 package com.example.budongbudong.domain.property.enums;
 
 public enum PropertyType {
-    APARTMENT,
-    VILLA,
-    OFFICETEL
+    APARTMENT("아파트"),
+    VILLA("빌라"),
+    OFFICETEL("오피스텔");
+
+    private final String description;
+
+    PropertyType(String description) {
+        this.description = description;
+    }
 }
