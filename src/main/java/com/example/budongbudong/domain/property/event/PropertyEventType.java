8873805e1454002107b2a18e3002ea6a9ecc1d93
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2026-01-27 09:05:00 +0900

    refactor: 주석 삭제

--- src/main/java/com/example/budongbudong/domain/property/event/PropertyEventType.java
+++ src/main/java/com/example/budongbudong/domain/property/event/PropertyEventType.java
@@ -1,8 +1,5 @@
 package com.example.budongbudong.domain.property.event;
 
-/**
- * 매물 변경 이벤트의 의미를 명확하게 표현하기 위한
- */
 public enum PropertyEventType {
     CREATED("매물 등록 이벤트"),
     UPDATED("매물 수정 이벤트"),
