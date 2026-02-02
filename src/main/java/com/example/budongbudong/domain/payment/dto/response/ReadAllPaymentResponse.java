e1bb97ad70b1eee17aa37462ea630345b4ce915f
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2026-02-02 16:10:00 +0900

    refactor: type 추가

--- src/main/java/com/example/budongbudong/domain/payment/dto/response/ReadAllPaymentResponse.java
+++ src/main/java/com/example/budongbudong/domain/payment/dto/response/ReadAllPaymentResponse.java
@@ -2,6 +2,7 @@ package com.example.budongbudong.domain.payment.dto.response;
 
 import com.example.budongbudong.common.entity.Payment;
 import com.example.budongbudong.domain.payment.enums.PaymentDisplayStatus;
+import com.example.budongbudong.domain.payment.enums.PaymentType;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReadAllPaymentResponse {
     private final Long paymentId;
+    private final PaymentType type;
     private final String orderName;
     private final BigDecimal amount;
     private final PaymentDisplayStatus status;
@@ -22,6 +24,7 @@ public class ReadAllPaymentResponse {
     public static ReadAllPaymentResponse from(Payment payment){
         return new ReadAllPaymentResponse(
                 payment.getId(),
+                payment.getType(),
                 payment.getOrderName(),
                 payment.getAmount(),
                 PaymentDisplayStatus.from(payment.getStatus()),
